58bec7218828f35488a5150c8613b9c76a137868
Chithra E P <chithraep1@gmail.com>
2026-06-19 20:56:21 +0100

    Add files via upload

---
+++ sql/sqlusp_insertAuditing.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROCEDURE dbo.usp_insertAuditing
+(
+    @PipelineName VARCHAR(100),
+    @FileName VARCHAR(100),
+    @Status VARCHAR(20),
+    @RowsProcessed INT,
+    @ExecutionDuration INT = NULL,
+    @ErrorMessage VARCHAR(500) = NULL
+)
+AS
+BEGIN
+
+    INSERT INTO Audit_Log
+    (
+        PipelineName,
+        FileName,
+        LoadDate,
+        Status,
+        RowsProcessed,
+        ExecutionDuration,
+        ErrorMessage
+    )
+    VALUES
+    (
+        @PipelineName,
+        @FileName,
+        GETDATE(),
+        @Status,
+        @RowsProcessed,
+        @ExecutionDuration,
+        @ErrorMessage
+    );
+
+END;
+GO
